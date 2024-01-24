d18a0ce35412e48d3569b382ccab1dd204dfc6c3
Luke Patrick <luke@routerchowder.com>
2024-01-24 09:45:44 -0600

    typo: left in a console log in isDarkColor utility

--- src/utils/isDarkColor/index.ts
+++ src/utils/isDarkColor/index.ts
@@ -2,7 +2,6 @@ import { hexToHSL } from '../paletteGenerator/hexConverters';
 
 const isDarkColor = (color: string): boolean => {
   const { l: luma } = hexToHSL(color);
-  console.log('luma is', luma);
 
   if (luma > 48) {
     return false;
