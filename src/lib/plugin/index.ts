e89d4d7d5c2cbfabc80995ceb94cb40ef7dbacf5
Luke Patrick <luke@routerchowder.com>
2024-01-24 08:49:05 -0600

    feat: expose default and overlay color values for easy text coloration based on passed theme values

--- src/lib/plugin/index.ts
+++ src/lib/plugin/index.ts
@@ -1,5 +1,6 @@
 import colors from 'tailwindcss/colors';
 import plugin from 'tailwindcss/plugin';
+import isDarkColor from '../../utils/isDarkColor';
 import paletteGenerator from '../../utils/paletteGenerator';
 
 interface SkogenOptions {
@@ -23,17 +24,19 @@ const skogen = plugin.withOptions(
     };
   },
   function (options: SkogenOptions) {
+    // determine default color based on options
+    const primary = options?.colors?.primary || colors.blue[400];
+    const secondary = options?.colors?.secondary || colors.violet[500];
+    // generate the theme extension
     return {
       theme: {
         extend: {
           // add custom colors
           colors: {
-            primary: paletteGenerator(
-              options?.colors?.primary || colors.blue[400],
-            ),
-            secondary: paletteGenerator(
-              options?.colors?.secondary || colors.violet[500],
-            ),
+            primary: paletteGenerator(primary),
+            secondary: paletteGenerator(secondary),
+            default: isDarkColor(primary) ? colors.white : colors.gray[900],
+            overlay: isDarkColor(primary) ? colors.white : colors.gray[900],
           },
           // add custom font selectors
           fontFamily: {
