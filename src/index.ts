914663eb8ba0c46232282019aff8e117ebb81042
Luke Patrick <luke@routerchowder.com>
2024-03-13 09:11:18 -0500

    feat: Create new Select component with rudimentary inputs and styling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { default as Button } from './components/Button';
 export { default as Divider } from './components/Divider';
 export { default as InputField } from './components/InputField';
+export { default as Select } from './components/Select';
 export { default as Typography } from './components/Typography';
 // utility exports
 export { default as paletteGenerator } from './utils/paletteGenerator';
