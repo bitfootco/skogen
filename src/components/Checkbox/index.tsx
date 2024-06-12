bda80796aeb0d1b057afa8bc31af0f32f00ef82a
Luke Patrick <luke@routerchowder.com>
2024-06-12 08:31:46 -0500

    feat: enable dark mode support for Checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -13,12 +13,12 @@ const Checkbox = ({ checked, label, onChange }: CheckboxProps) => {
         checked={checked}
         id="default-checkbox"
         type="checkbox"
-        className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-blue-500"
+        className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
         onChange={onChange}
       />
       <label
         htmlFor="default-checkbox"
-        className="ms-2 text-sm font-medium text-gray-900"
+        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-200"
       >
         {label}
       </label>
