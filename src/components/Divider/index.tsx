705f21bcc63c559509660074ec8e781b03a13392
Luke Patrick <luke@routerchowder.com>
2024-06-12 15:44:00 -0500

    refactor: drive Divider color off dark mode rather than an arbitrary prop dark = true

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -4,14 +4,9 @@ import { twMerge } from 'tailwind-merge';
 interface DividerProps {
   className?: string;
   size?: 'sm' | 'md' | 'lg' | 'xl';
-  dark?: boolean;
 }
 
-const Divider = ({
-  className = '',
-  size = 'md',
-  dark = true,
-}: DividerProps) => {
+const Divider = ({ className = '', size = 'md' }: DividerProps) => {
   const sizesDictionary = {
     sm: 'h-px',
     md: 'h-0.5',
@@ -19,12 +14,10 @@ const Divider = ({
     xl: 'h-2',
   };
 
-  const color = dark ? 'bg-gray-300' : 'bg-gray-100';
-
   return (
     <div
       className={twMerge(
-        `mx-8 my-4 flex w-full border-t-0 opacity-100 ${color} ${sizesDictionary[size]} ${className}`,
+        `mx-8 my-4 flex w-full border-t-0 bg-gray-100 opacity-100 dark:bg-gray-300 ${sizesDictionary[size]} ${className}`,
       )}
     />
   );
