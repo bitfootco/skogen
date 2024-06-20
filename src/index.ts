6c214b6a71b960311c353c61b785bf2fd4880842
Luke Patrick <luke@routerchowder.com>
2024-06-20 08:45:25 -0500

    chore: expose the new InputMask component in the default export list

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as Button } from './components/Button';
 export { default as Checkbox } from './components/Checkbox';
 export { default as Divider } from './components/Divider';
 export { default as InputField } from './components/InputField';
+export { default as InputMask } from './components/InputMask';
 export { default as Select } from './components/Select';
 export { default as Typography } from './components/Typography';
 // utility exports
