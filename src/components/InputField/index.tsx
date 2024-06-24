59b2ea56ebffd3ad3b9a9931f28699fee179d521
Luke Patrick <luke@routerchowder.com>
2024-06-24 09:06:43 -0500

    refactor: improve performance of InputMask component and modify InputField to accept a ref

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import Typography from '../Typography';
 
@@ -15,57 +17,61 @@ interface InputFieldProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField = ({
-  label,
-  placeholder,
-  id,
-  value,
-  size = 'md',
-  required = false,
-  disabled = false,
-  error = '',
-  className = '',
-  onChange = () => {},
-}: InputFieldProps) => {
-  // 1. Create a dictionary to map the size prop to the tailwind classes
-  const sizeDictionary = {
-    sm: 'p-2.5 text-sm',
-    md: 'p-3 text-sm',
-    lg: 'p-4 text-base',
-  };
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+  (
+    {
+      label,
+      placeholder,
+      id,
+      value,
+      size = 'md',
+      required = false,
+      disabled = false,
+      error = '',
+      className = '',
+      onChange = () => {},
+    },
+    ref,
+  ) => {
+    const sizeDictionary = {
+      sm: 'p-2.5 text-sm',
+      md: 'p-3 text-sm',
+      lg: 'p-4 text-base',
+    };
 
-  // 2. Create a border styling based on the error prop
-  const borderStyling = error
-    ? 'border-red-500 focus:border-red-500 focus:ring-red-500 dark:border-red-500 dark:focus:border-red-500 dark:focus:ring-red-500'
-    : 'border-gray-300 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500';
+    const borderStyling = error
+      ? 'border-red-500 focus:border-red-500 focus:ring-red-500 dark:border-red-500 dark:focus:border-red-500 dark:focus:ring-red-500'
+      : 'border-gray-300 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500';
 
-  return (
-    <form>
-      <label
-        htmlFor={id}
-        className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-200"
-      >
-        {label}
-      </label>
-      <input
-        type="text"
-        id={id}
-        value={value}
-        className={twMerge(
-          `block w-full rounded-lg border bg-gray-50 p-2.5 text-sm text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 ${sizeDictionary[size]} ${borderStyling} ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'} ${className}`,
+    return (
+      <div>
+        <label
+          htmlFor={id}
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-200"
+        >
+          {label}
+        </label>
+        <input
+          type="text"
+          id={id}
+          value={value}
+          className={twMerge(
+            `block w-full rounded-lg border bg-gray-50 p-2.5 text-sm text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 ${sizeDictionary[size]} ${borderStyling} ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'} ${className}`,
+          )}
+          placeholder={placeholder}
+          required={required}
+          disabled={disabled}
+          onChange={onChange}
+          ref={ref}
+        />
+        {error && (
+          <Typography variant="p" color="error" className="mb-2">
+            {error}
+          </Typography>
         )}
-        placeholder={placeholder}
-        required={required}
-        disabled={disabled}
-        onChange={onChange}
-      />
-      {error && (
-        <Typography variant="p" color="error" className="mb-2">
-          {error}
-        </Typography>
-      )}
-    </form>
-  );
-};
+      </div>
+    );
+  },
+);
 
 export default InputField;
--- src/components/InputMask/index.tsx
+++ src/components/InputMask/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+'use client';
+
+import React, { useRef } from 'react';
 import InputField from '../InputField';
 
 type InputMaskProps = {
@@ -6,63 +8,98 @@ type InputMaskProps = {
   mask: string;
   label: string;
   maskPlaceholder?: string;
-  value?: string;
-  onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  onChange: (input: string) => void;
 };
 
 const InputMask: React.FC<InputMaskProps> = ({
   id,
   mask,
   label,
-  maskPlaceholder = '_',
+  maskPlaceholder = '#',
   value = '',
   onChange,
 }) => {
-  const [maskedValue, setMaskedValue] = useState('');
-
-  useEffect(() => {
-    if (value) {
-      setMaskedValue(applyMask(value, mask, maskPlaceholder));
-    }
-  }, [value, mask, maskPlaceholder]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const rawValue = event.target.value;
-    const newMaskedValue = applyMask(rawValue, mask, maskPlaceholder);
-    setMaskedValue(newMaskedValue);
-    if (onChange) {
-      onChange(event);
-    }
+    const rawValue = event.target.value.replace(/\D/g, ''); // Remove all non-digit characters
+    const cursorPosition = event.target.selectionStart ?? 0;
+    const newMaskedValue = applyMask(rawValue);
+    const adjustedCursorPosition = calculateCursorPosition(
+      event.target.value,
+      newMaskedValue,
+      cursorPosition,
+    );
+    onChange(newMaskedValue);
+
+    setTimeout(() => {
+      if (inputRef.current) {
+        inputRef.current.setSelectionRange(
+          adjustedCursorPosition,
+          adjustedCursorPosition,
+        );
+      }
+    }, 0);
   };
 
-  const applyMask = (value: string, mask: string, placeholder: string) => {
+  const applyMask = (value: string) => {
     let maskedValue = '';
     let valueIndex = 0;
+
     for (let i = 0; i < mask.length; i++) {
-      if (mask[i] === placeholder) {
+      if (mask[i] === maskPlaceholder) {
         if (value[valueIndex]) {
           maskedValue += value[valueIndex];
           valueIndex++;
         } else {
-          maskedValue += placeholder;
+          maskedValue += maskPlaceholder;
         }
       } else {
         maskedValue += mask[i];
-        if (mask[i] === value[valueIndex]) {
-          valueIndex++;
-        }
       }
     }
+
     return maskedValue;
+  };
+
+  const calculateCursorPosition = (
+    rawInput: string,
+    maskedValue: string,
+    cursorPosition: number,
+  ) => {
+    let maskIndex = 0;
+    let rawIndex = 0;
+
+    while (maskIndex < cursorPosition) {
+      if (mask[maskIndex] === maskPlaceholder) {
+        if (rawInput[maskIndex] && rawInput[maskIndex].match(/\d/)) {
+          rawIndex++;
+        }
+      }
+      maskIndex++;
+    }
+
+    // Adjust cursor position to ensure it doesn't skip over mask characters
+    let newCursorPosition = rawIndex;
+    while (
+      maskedValue[newCursorPosition] &&
+      maskedValue[newCursorPosition] !== maskPlaceholder
+    ) {
+      newCursorPosition++;
+    }
+
+    return newCursorPosition;
   };
 
   return (
     <InputField
       id={id}
       label={label}
-      placeholder={''}
-      value={maskedValue}
+      placeholder={mask}
+      value={value}
       onChange={handleChange}
+      ref={inputRef}
     />
   );
 };
