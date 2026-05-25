6e6b72423ee2481adc2d6711d0c60f9c44387fad
Luke Patrick <luke@bitfoot.co>
2026-05-25 18:24:07 +1200

    fix: correct storybook issue blocking that deploy + unused React whoopsie

--- src/components/Toaster/index.tsx
+++ src/components/Toaster/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { subscribe, removeToast } from '../../utils/toastStore';
 import type { ToastItem } from '../../utils/toastStore';
 import Toast from '../Toast';
